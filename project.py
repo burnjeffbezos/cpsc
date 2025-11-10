fae6ee002ea3f2b11d2f14836fdfc07167be0826
dan <robbierottenakashrek@gmail.com>
2025-11-10 18:12:37 -0500

    starting defining functions

--- project.py
+++ project.py
@@ -1,1 +1,12 @@
 print("Hi!")
+#
+#
+# real code below
+def main():
+    num_input = input("Please enter a number in either decimal or hexidecimal. ")
+    return num_input
+#
+#
+the_real_input = main()
+print(the_real_input)
+#
