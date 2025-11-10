beabefe33aef2cfdf9f8ef648dd76e60948a6f51
dan <robbierottenakashrek@gmail.com>
2025-11-10 18:40:12 -0500

    added hex T/F checker

--- project.py
+++ project.py
@@ -10,9 +10,13 @@ def main():
 the_real_input = main()
 print(the_real_input)
 #
+#
 def parse_input(num_input):
     is_hex_YN = False
     if 'x' in num_input:
         is_hex_YN = True
     return is_hex_YN
-    
+#
+#
+the_real_truth = parse_input(the_real_input)
+print(the_real_truth)
