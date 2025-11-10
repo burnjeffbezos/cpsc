e3ca0a8fc8776a43cbebc275a7aa6640cf2a6bc0
dan <robbierottenakashrek@gmail.com>
2025-11-10 18:54:57 -0500

    mwow

--- project.py
+++ project.py
@@ -20,3 +20,21 @@ def parse_input(num_input):
 #
 the_real_truth = parse_input(the_real_input)
 print(the_real_truth)
+#
+#
+def convert_to_binary(unconverted_number):
+    # this code converts hex or decimal to binary
+    # pseudo code below
+    #
+    # if real truth = true
+    #   this is a hex number
+    #   convert this to binary
+    #   converted number =
+    # if real truth = false
+    #   this is a decimal number
+    #   convert this to binary
+    #   converted number =
+    # return converted_number
+    pass
+#
+#
