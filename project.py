a9e7a8b568b41dd930969dd93800d6142c7d90af
dan <robbierottenakashrek@gmail.com>
2025-11-10 20:02:47 -0500

    + test code
    added test doe and cleaned up some things

--- project.py
+++ project.py
@@ -1,13 +1,9 @@
-print("Hi!")
-#
-#
-# real code below
 def main():
     num_input = input("Please enter a number in either decimal or hexidecimal. ")
     return num_input
 #
 #
-# TEST: REMOVE BEFORE UPLOADING
+#
 the_real_input = main()
 print(the_real_input)
 #
@@ -19,17 +15,16 @@ def parse_input(num_input):
     return is_hex_YN
 #
 #
-# TEST: REMOVE BEFORE UPLOADING
 the_real_truth = parse_input(the_real_input)
-print(the_real_truth)
 #
 #
 def decimal_to_binary(dec):
     binary = bin(int(dec)).replace("0b", "")
     return binary
 #
-def hex_to_binary():
-    pass
+def hex_to_binary(hex):
+    binary = hex
+    return binary
 #
 #
 def convert_to_binary(unconverted_number):
@@ -40,3 +35,13 @@ def convert_to_binary(unconverted_number):
     return converted_number
 #
 #
+the_real_output = convert_to_binary(the_real_input)
+#
+#
+# Testing code below
+def test():
+    print(f"Your input: {the_real_input}")
+    print(f"Number is Hex: {the_real_truth}")
+    print(f"Your converted number is: {the_real_output}")
+#
+test()
